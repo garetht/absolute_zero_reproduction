68d9b7ca4a2e277e43dd1ce3556fcccbc678b457
Lucy Wingard <lucywingard97@gmail.com>
2025-05-27 15:20:46 +0100

    return logprobs with proper dims

--- model/inference.py
+++ model/inference.py
@@ -3,9 +3,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from model.args import AZRArgs
 
 # returns the str response and the logprobs for the response
-def generate_response(args: AZRArgs, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: str) -> tuple[str, Float[torch.Tensor, "vocab"]]:
-    return generate_response_bulk(args, model, tokenizer, [prompt])[0]
+def generate_response(args: AZRArgs, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: str) -> tuple[str, Float[torch.Tensor, "seq_len vocab"]]:
+    responses, logprobs = generate_response_bulk(args, model, tokenizer, [prompt])
+    return responses[0], logprobs[0]
 
-# for each prompt in the list, returns a tuple of the str response and the logprobs for the response
-def generate_response_bulk(args: AZRArgs, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: list[str]) -> list[tuple[str, Float[torch.Tensor, "vocab"]]]:
+# for each prompt in the list, returns a tuple of lists: (list of str responses, list of logprobs tensors)
+def generate_response_bulk(args: AZRArgs, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: list[str]) -> tuple[list[str], Float[torch.Tensor, "batch_size seq_len vocab"]]:
     pass
