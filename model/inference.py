682d5c22ce6b3723dad94ed17ee268ebc0fde088
Lucy Wingard <lucywingard97@gmail.com>
2025-05-27 13:40:11 +0100

    update inference to return logprobs

--- model/inference.py
+++ model/inference.py
@@ -2,10 +2,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from model.args import AZRArgs
 
-
-def generate_response(args: AZRArgs, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: str) -> str:
+# returns the str response and the logprobs for the response
+def generate_response(args: AZRArgs, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: str) -> tuple[str, Float[torch.Tensor, "vocab"]]:
     return generate_response_bulk(args, model, tokenizer, [prompt])[0]
 
-
-def generate_response_bulk(args: AZRArgs, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: list[str]) -> list[str]:
+# for each prompt in the list, returns a tuple of the str response and the logprobs for the response
+def generate_response_bulk(args: AZRArgs, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, prompt: list[str]) -> list[tuple[str, Float[torch.Tensor, "vocab"]]]:
     pass
