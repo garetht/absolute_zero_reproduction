3e6530759ad9c423ba71dd3bc01405200d591d3d
Lucy Wingard <lucywingard97@gmail.com>
2025-05-28 10:07:58 +0100

    add objective computation

--- model/trainer.py
+++ model/trainer.py
@@ -15,8 +15,8 @@ from buffer.base_buff import BaseBuffer, MegaBuffer,
 from model.args import AZRArgs
 from model.compute.advantages import compute_advantages
 from model.compute.reward import compute_r_total
-from model.inference import generate_response, generate_response_bulk
-from custom_types import Answer, TaskType, Role, BaseSample, IOPair
+from model.inference import generate_response, generate_response_bulk, remove_dvocab_from_logprobs
+from custom_types import Answer, MiniBatch, TaskType, Role, BaseSample, IOPair
 from utils.validate_by_executing import validate_by_executing_induction, validate_by_executing_deduction_abduction
 from utils.string_formatting import format_task_prompts, format_for_abduction, format_for_induction
 
@@ -82,14 +82,21 @@ class AZRTrainer:
         self.mega_buffer = mega_buffer
         self.step = 0
 
-    def compute_azr_objective(self, advantages: Float[torch.Tensor, "task batch_size"], new_logprobs: Float[torch.Tensor, "role task minibatch_size seq_len vocab_size"], new_sample_ids:  Int[torch.Tensor, "role task minibatch_size seq_len"] ) -> Float[torch.Tensor, ""]:
+    def compute_azr_objective(self, advantages: Float[torch.Tensor, "role task minibatch_size"], new_logprobs: Float[torch.Tensor, "role task minibatch_size seq_len"], new_sample_ids:  Int[torch.Tensor, "role task minibatch_size seq_len"], minibatch: MiniBatch ) -> Float[torch.Tensor, ""]:
         """
         Compute the AZR training objective.
         
         Returns:
             Float[torch.Tensor, ""]: The computed objective value as a scalar tensor.
         """
-        pass
+        # compute the importance ratio using logprobs and sample_ids
+        # using the new sample_ids and the old logprobs, get the logprobs from the old policy for the new sample_ids
+        old_logprobs = minibatch.logprobs.gather(-1, new_sample_ids.unsqueeze(-1)).squeeze(-1)  # shape: (role, task, minibatch_size, seq_len)
+        importance_ratio = (new_logprobs - old_logprobs).exp()  # shape: (role, task, minibatch_size, seq_len)
+        non_clipped = advantages * importance_ratio  # shape: (role, task, minibatch_size, seq_len, )
+        # compute the clipped objective
+        clipped = advantages.clamp(-self.args.clip_ratio, self.args.clip_ratio) * importance_ratio # shape: (role, task, minibatch_size, seq_len, 
+        return torch.min(non_clipped, clipped).mean()  # shape: ()
 
     def rollout_phase(self) -> Float[torch.Tensor, "role task batch_size"]:
         """
@@ -177,7 +184,7 @@ class AZRTrainer:
         return all_rewards  # shape: (role, task, batch_size)
 
 
-    def propose_task(self, task_type: TaskType) -> tuple[str, Float[torch.Tensor, "seq_len vocab"], Int[torch.Tensor, "seq_len"]]
+    def propose_task(self, task_type: TaskType) -> tuple[str, Float[torch.Tensor, "seq_len vocab_size"], Int[torch.Tensor, "seq_len"]]:
         sample = self.mega_buffer.sample_abduction_deduction()
         match task_type:
             case TaskType.ABDUCTION:
@@ -191,7 +198,7 @@ class AZRTrainer:
         return response, logprobs, sample_ids
 
 
-    def generate_io_pairs(self, program: BaseSample, num_io_pairs: int) -> tuple[list[IOPair], Float[torch.Tensor, "seq_len vocab"], Int[torch.Tensor, "seq_len"]]:
+    def generate_io_pairs(self, program: BaseSample, num_io_pairs: int) -> tuple[list[IOPair], Float[torch.Tensor, "seq_len vocab_size"], Int[torch.Tensor, "seq_len"]]:
         induction_prompt = format_for_induction(program, num_io_pairs)
         response, logprobs, sample_ids, prompt_tokens = generate_response(self.args, self.training_model, self.tokenizer, induction_prompt)
         io_pairs = extract_io_pairs_from_string(response, num_io_pairs)
@@ -219,8 +226,10 @@ class AZRTrainer:
             _, logprobs, sample_ids, _ = generate_response_bulk(
                 self.args, self.training_model, self.tokenizer, prompts
             )
-            advantages = compute_advantages(self.args, all_rewards) # shape task batch_size
-            objective = self.compute_azr_objective(advantages, logprobs, sample_ids)
+            logprobs = remove_dvocab_from_logprobs(logprobs, sample_ids)
+            advantages = compute_advantages(self.args, all_rewards) # shape role task minibatch_size
+            objective = self.compute_azr_objective(advantages, logprobs, sample_ids, 
+                                                   mini_batch)
             self.optimizer.zero_grad()
             objective.backward()
             torch.nn.utils.clip_grad_norm_(self.training_model.parameters(), self.args.max_grad_norm)
