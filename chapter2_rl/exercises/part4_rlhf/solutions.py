954898776a362cc1afbf4f0fe59086e54a315b74
Gareth Tan <garetht@users.noreply.github.com>
2025-05-29 21:05:34 +0100

    Changes.

--- chapter2_rl/exercises/part4_rlhf/solutions.py
+++ chapter2_rl/exercises/part4_rlhf/solutions.py
@@ -84,7 +84,7 @@ class RLHFArgs:
 
     # Base model & sampling arguments
     base_model: str = BASE_MODEL
-    gen_len: int = 50
+    gen_len: int = 40
     temperature: float = 1.0
     top_k: int = 10
     prefix: str = create_solver_prompt(PROBLEM)
